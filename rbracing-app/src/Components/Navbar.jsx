7c52d81ed96ff701b9203e0afad4e28fbf0f9b5b
Fernando Daniel Lucero <98407078+Ferluc@users.noreply.github.com>
2023-07-10 17:52:46 -0300

    Update logo y cart

--- rbracing-app/src/Components/Navbar.jsx
+++ rbracing-app/src/Components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import './Navbar.css'
-
+import logo from '../Logo/RBlogo.png';
+import cart from '../Logo/Cart.png';
 const Navbar = ({menuItems}) =>{
    
 
@@ -10,12 +11,14 @@ const Navbar = ({menuItems}) =>{
     <nav >
         
            <div className="navbar">
+           <img src={logo} className='navbar-logo' />
         <ul id="menuItems">
             <li><a href="index.html">Verstappen</a></li>
             <li><a href="index.html">Checo</a></li>
             <li><a href="index.html">Fanwear</a></li>
             <li><a href="index.html">Headwear</a></li>
             <li><a href="index.html">Purchase</a></li>
+            <img src={cart} className='navbar-logo' />
         </ul>
         </div> 
         
